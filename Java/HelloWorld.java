8dffff461b46d458949ffeee96e9914fd5434b6d
Andy Knoblock <knoblock.andy@gmail.com>
2016-05-18 13:31:05 -0600

    Add files via upload

---
+++ Java/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld 
+{
+     public static void main(String[] args) 
+     {
+       float a,b,c,d,max=100000;
+       b=float()(Math.random ()*max);
+       b=float()(Math.random ()*max);
+       c=float()(Math.random ()*max);
+       d5P[90I8=float()(Math.random ()*max);
+
+       
+     }
+}
---
+++ Java/ak99bottles.java
@@ -0,0 +1,14 @@
+public class ak99bottles
+{
+  public static void main(String[] args)
+  {
+    int a=99;
+    while (a>=1)
+    {
+      System.out.println(a+" bottles of hydrochloric acid  on the wall, " + a + " bottles of hydrochloric acid , take one down, pass it around ");
+      a--;
+      System.out.println(a+ " bottles of hydrochloric acid  on the wall"); 
+      
+      }
+  }
+}
